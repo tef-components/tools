47fe4a09d3a60b669ae1b93a37f2f82429a526de
Arnau March <arnau@arnaumarch.com>
2015-04-09 16:14:04 +0200

    updated layout functionality

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Set of tools to help you develop your components.
 
 This [demo](http://tef-components.github.io/tools/index.html) will help you testing your breakpoints in all themes.
+
+Read our [docs section](https://github.com/tef-components/docs) to learn how to use TEF Components.
--- js/layout.js
+++ js/layout.js
@@ -8,6 +8,7 @@ $(function() {
     $('#add-columns-modal').removeClass('show');
 
     var cols = $('#add-columns-number').val();
+    $('#main-content').append('<labe id="main-grid-lbl"></labe><div id="main-grid"></div>');
     $('#main-grid').attr('columns', cols);
     if ($('input[name=add-columns-gutter]:checked').val() === 'yes') {
       $('#main-grid').addClass('tef-gutter');
@@ -15,17 +16,33 @@ $(function() {
 
     // build columns
     for (var i = 0; i < cols ; ++ i) {
-      $('#main-grid').append('<div id=column-'+ (i+1) +'"><span>Column '+ (i+1) +'</span> </div>');
+      $('#main-grid').append('<div id="column-'+ (i+1) +'"><span>&lt;div&gt;</span></div>');
     }
-  });
+    $('#main-grid-lbl').text('<div columns="'+ i +'">');
+    
+    // Select columns
+    $('#main-grid').click(function(e) {
+      $('#main-grid').removeClass('active').find('.active').removeClass('active');
+      if ($(e.target).attr('id')) {
+        $(e.target).addClass('active');
+      } else {
+        $(e.target).closest('[id]').addClass('active');
+      }
 
-  // Select columns
-  $('#main-grid').click(function(e) {
-    $('#main-grid *').removeClass('active');
-    if ($(e.target).attr('id')) {
-      $(e.target).addClass('active');
-    } else {
-      $(e.target).closest('[id]').addClass('active');
-    }
+      // enable actions
+      $('#add-nested-columns').removeAttr('disabled');
+      $('#remove-column').removeAttr('disabled');
+    });
+
+    // Remove column
+    $('#remove-column').click(function(e) {
+      if($('#main-grid').hasClass('active') == true) {
+        $('#main-grid').remove();
+        $('#main-grid-lbl').remove();
+        $('#add-columns-modal').addClass('show');
+      } else {
+        $('#main-grid .active').remove();
+      }
+    });
   });
 });
