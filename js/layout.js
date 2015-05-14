aa3ad7642e6f8626b93ab0cc1758d7b304045aaf
Arnau March <arnau@arnaumarch.com>
2015-05-14 09:29:41 +0200

    restructured components

--- js/layout.js
+++ js/layout.js
@@ -1,26 +1,26 @@
 $(function() {
 
   // Launch modal on load
-  $('#add-columns-modal').addClass('show');
+  $('#add-data-columns-modal').addClass('show');
 
   // create main layout
-  $('#add-columns-btn').click(function() {
-    $('#add-columns-modal').removeClass('show');
+  $('#add-data-columns-btn').click(function() {
+    $('#add-data-columns-modal').removeClass('show');
 
-    var cols = $('#add-columns-number').val();
+    var cols = $('#add-data-columns-number').val();
     $('#main-content').append('<labe id="main-grid-lbl"></labe><div id="main-grid"></div>');
-    $('#main-grid').attr('columns', cols);
-    if ($('input[name=add-columns-gutter]:checked').val() === 'yes') {
-      $('#main-grid').addClass('tef-gutter');
+    $('#main-grid').attr('data-columns', cols);
+    if ($('input[name=add-data-columns-gutter]:checked').val() === 'yes') {
+      $('#main-grid').addClass('tc-gutter');
     }
 
-    // build columns
+    // build data-columns
     for (var i = 0; i < cols ; ++ i) {
-      $('#main-grid').append('<div id="column-'+ (i+1) +'"><span>&lt;div&gt;</span></div>');
+      $('#main-grid').append('<div id="data-column-'+ (i+1) +'"><span>&lt;div&gt;</span></div>');
     }
-    $('#main-grid-lbl').text('<div columns="'+ i +'">');
+    $('#main-grid-lbl').text('<div data-columns="'+ i +'">');
     
-    // Select columns
+    // Select data-columns
     $('#main-grid').click(function(e) {
       $('#main-grid').removeClass('active').find('.active').removeClass('active');
       if ($(e.target).attr('id')) {
@@ -30,16 +30,16 @@ $(function() {
       }
 
       // enable actions
-      $('#add-nested-columns').removeAttr('disabled');
-      $('#remove-column').removeAttr('disabled');
+      $('#add-nested-data-columns').removeAttr('disabled');
+      $('#remove-data-column').removeAttr('disabled');
     });
 
-    // Remove column
-    $('#remove-column').click(function(e) {
+    // Remove data-column
+    $('#remove-data-column').click(function(e) {
       if($('#main-grid').hasClass('active') == true) {
         $('#main-grid').remove();
         $('#main-grid-lbl').remove();
-        $('#add-columns-modal').addClass('show');
+        $('#add-data-columns-modal').addClass('show');
       } else {
         $('#main-grid .active').remove();
       }
